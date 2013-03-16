a36d38ee53734041f10720e356a35d34936d9f5c
Abhishek Chouhan <abhishekchouhan@gmail.com>
2013-03-16 20:47:17 +0800

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vastipatrak::Application.config.secret_token = '9bd2ed081babe045fa9e8a9121a331ae651fdb65a92069b538edfee239f31652d803695792288216c6ca4a9b20239478326f7ac5332d4b8dd795fcdde25ff9d9'
